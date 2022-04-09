9a559061aacc7959676f88cbd551a1c3fddd373f
rmslk <102646325+rmslk@users.noreply.github.com>
2022-04-09 22:59:26 +0300

    Создание файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,11 @@
+# A/B калькулятор
+
+import tkinter as tk
+
+# Создание главного окнаэ
+root = tk.Tk()
+root.geometry("280x300")
+root.title("A/B калькулятор")
+
+# Запуск цикла mainloop
+root.mainloop()
